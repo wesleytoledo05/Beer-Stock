5d97b93727b6ba3a1e88ba6ea3a160a3dded41d5
wesleytoledo05 <vntoledo05@gmail.com>
2021-07-27 14:03:45 -0300

    Realização do projeto de Desenvolvimento de testes unitários

---
+++ src/main/java/com/dio/beerstock/entity/Beer.java
@@ -0,0 +1,34 @@
+package com.dio.beerstock.entity;
+
+import com.dio.beerstock.enums.BeerType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Beer {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private int max;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Enumerated(EnumType.STRING) 
+    @Column(nullable = false)
+    private BeerType type;
+}
---
+++ src/test/java/com/dio/beerstock/service/BeerServiceTest.java
@@ -0,0 +1,167 @@
+package com.dio.beerstock.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import com.dio.beerstock.DTO.BeerDTO;
+import com.dio.beerstock.builder.BeerBuilder;
+import com.dio.beerstock.entity.Beer;
+import com.dio.beerstock.exception.BeerAlreadyRegisteredException;
+import com.dio.beerstock.exception.BeerNotFoundException;
+import com.dio.beerstock.mapper.BeerMapper;
+import com.dio.beerstock.repository.BeerRepository;
+
+import org.assertj.core.api.Assertions;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName(value = "Teste for BeerService")
+class BeerServiceTest {
+
+    private static final Long INVALID_BEER_ID = 1l;
+
+    @InjectMocks
+    private BeerService beerService;
+    private BeerMapper beerMapper = BeerMapper.INSTANCE;
+
+    @Mock   // sempre da classe que pretendemos "simular"
+    private BeerRepository beerRepositoryMock;
+
+//    @BeforeEach // compilar antes de todos testes
+//    void setUp() {
+//        // prepara o ambiente, o que é "fornecido"
+//        BeerDTO beerDTO = BeerBuilder.builder().build().toBeerDTO();
+//        Beer expectedSavedBeer = beerMapper.toModel(beerDTO);
+//
+//        // o que fazer quando chamar a classe mockada (simulada)
+//        BDDMockito.when(beerRepositoryMock.findByName(beerDTO.getName()))
+//                .thenReturn(Optional.of(expectedSavedBeer));
+//        BDDMockito.when(beerRepositoryMock.save(expectedSavedBeer))
+//                .thenReturn(expectedSavedBeer);
+//    }
+
+    @Test
+    @DisplayName(value = "Should create a beer when it is informed")
+    void whenBeerInformedThenItShouldBeCreated() throws BeerAlreadyRegisteredException {
+        // given
+        BeerDTO givenBeer = BeerBuilder.builder().build().toBeerDTO();
+        Beer expectedBeer = beerMapper.toModel(givenBeer);
+
+        // when
+        BDDMockito.when(beerRepositoryMock.findByName(givenBeer.getName())).thenReturn(Optional.empty());
+        BDDMockito.when(beerRepositoryMock.save(ArgumentMatchers.any(Beer.class))).thenReturn(expectedBeer);
+
+        // then
+        BeerDTO testedBeer = beerService.createBeer(givenBeer);
+
+        MatcherAssert.assertThat(testedBeer.getId(),
+                Matchers.is(Matchers.equalTo(givenBeer.getId())));
+
+        Assertions.assertThat(testedBeer).isNotNull().isEqualTo(givenBeer);
+        Assertions.assertThat(testedBeer.getQuantity()).isGreaterThan(2);
+
+        assertEquals(givenBeer.getId(), testedBeer.getId());
+        assertEquals(givenBeer.getName(), testedBeer.getName());
+    }
+
+    @Test
+    void whenAlreadyRegisteredBeerInformedThenExceptionShouldBeThrown() {
+        // given
+        BeerDTO givenBeer = BeerBuilder.builder().build().toBeerDTO();
+        Beer duplicatedBeer = beerMapper.toModel(givenBeer);
+
+        // when
+        BDDMockito.when(beerRepositoryMock.findByName(givenBeer.getName()))
+                .thenReturn(Optional.of(duplicatedBeer));
+
+        // then
+        assertThrows(BeerAlreadyRegisteredException.class, () -> beerService.createBeer(givenBeer));
+    }
+
+    @Test
+    void whenValidBeerNameIsGivenThenReturnABeer() throws BeerNotFoundException {
+        // given
+        BeerDTO givenBeerDTO = BeerBuilder.builder().build().toBeerDTO();
+        Beer givenBeer = beerMapper.toModel(givenBeerDTO);
+        
+        // when
+        BDDMockito.when(beerRepositoryMock.findByName(givenBeer.getName()))
+                .thenReturn(Optional.of(givenBeer));
+        
+        // then
+        BeerDTO foundBeerDTO = beerService.findByName(givenBeerDTO.getName());
+
+        Assertions.assertThat(foundBeerDTO).isEqualTo(givenBeerDTO);
+    }
+
+    @Test
+    void whenNotRegisteredBeerNameIsGivenThenThrownAnException() {
+        // given
+        BeerDTO givenBeerDTO = BeerBuilder.builder().build().toBeerDTO();
+
+        // when
+        BDDMockito.when(beerRepositoryMock.findByName(givenBeerDTO.getName()))
+                .thenReturn(Optional.empty());
+
+        // then
+        assertThrows(BeerNotFoundException.class, () -> beerService.findByName(givenBeerDTO.getName()));
+    }
+
+    @Test
+    void whenListBeerIsCalledThenReturnAListOfBeers() {
+        // given
+        BeerDTO givenBeerDTO = BeerBuilder.builder().build().toBeerDTO();
+        Beer givenBeer = beerMapper.toModel(givenBeerDTO);
+
+        // when
+        BDDMockito.when(beerRepositoryMock.findAll()).thenReturn(Collections.singletonList(givenBeer));
+
+        // then
+        List<BeerDTO> foundBeerDTO = beerService.listAll();
+
+        Assertions.assertThat(foundBeerDTO).isNotEmpty();
+        Assertions.assertThat(foundBeerDTO.get(0)).isEqualTo(givenBeerDTO);
+
+    }
+
+    @Test
+    void whenListBeerIsCalledThenReturnAnEmptyList() {
+        // when
+        BDDMockito.when(beerRepositoryMock.findAll()).thenReturn(Collections.EMPTY_LIST);
+
+        // then
+        List<BeerDTO> foundBeerDTO = beerService.listAll();
+
+        Assertions.assertThat(foundBeerDTO).isEmpty();
+    }
+
+    @Test
+    void whenExclusionIsCalledWithValidIdThenABeerShouldBeDeleted() throws BeerNotFoundException {
+        // given
+        BeerDTO givenBeerDTO = BeerBuilder.builder().build().toBeerDTO();
+        Beer givenBeer = beerMapper.toModel(givenBeerDTO);
+
+        // when
+        BDDMockito.when(beerRepositoryMock.findById(givenBeerDTO.getId())).thenReturn(Optional.of(givenBeer));
+        BDDMockito.doNothing().when(beerRepositoryMock).deleteById(givenBeerDTO.getId());
+        // then
+        beerService.deleteById(givenBeerDTO.getId());
+
+        BDDMockito.verify(beerRepositoryMock, Mockito.times(1)).findById(givenBeer.getId());
+        BDDMockito.verify(beerRepositoryMock, Mockito.times(1)).deleteById(givenBeer.getId());
+    }
+}
